3d4f3b9bbdef46bc009b4cbff1d89f00a008e5f5
Patricio Tapia <native.devil@gmail.com>
2025-07-09 00:03:55 -0400

    REFACT: LogEntity constructor params as interface

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,22 +4,38 @@ export enum LogSeverityLevel {
   high = 'high'
 }
 
+export interface LogEntityOptions {
+  message: string;
+  level: LogSeverityLevel;
+  origin: string;
+  createdAt?: Date;
+}
+
 export class LogEntity {
   public level: string; //Enum
   public message: string;
   public createdAt: Date;
+  public origin: string;
 
-  constructor(message:string, level:LogSeverityLevel){
+  constructor(options: LogEntityOptions){
+    const { message, level, createdAt = new Date(), origin } = options;
     this.message = message;
     this.level = level;
-    this.createdAt = new Date();
+    this.createdAt = createdAt;
+    this.origin = origin;
   }
 
   static fromJSON = (json: string): LogEntity => {
     const { message, level, createdAt } = JSON.parse(json);
     if( !message ) throw new Error('Message is required');
     
-    const log = new LogEntity(message, level);
+    const log = new LogEntity({
+      message: message,
+      level: level,
+      createdAt: createdAt,
+      origin: 'log.entity.ts'
+    });
+
     log.createdAt = new Date(createdAt);
     return log;
   }
