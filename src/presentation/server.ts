e1767aff0418583cf11f5da6396c27f8a95a0620
Patricio Tapia <native.devil@gmail.com>
2025-07-09 00:07:22 -0400

    REFACT: email as env var

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugins";
 import { FileSystemDataSource } from "../domain/datasources/file-system.datasource";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { LogRepositoryImplementation } from "../infrastructure/repository/log-repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImplementation(
   new FileSystemDataSource()
@@ -10,7 +12,10 @@ const fileSystemLogRepository = new LogRepositoryImplementation(
 export class Server {
   static start(){
     console.log('Server started...');
+    this.sendLogFilesByEmail( envs.DEFAULT_RECEIVER_EMAIL );
+  }
 
+  static checkLocalApi = () => {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
@@ -22,6 +27,22 @@ export class Server {
         ).execute( url );
       }
     );
+  }
 
+  static sendTestEmail = () => {
+    const emailService = new EmailService(fileSystemLogRepository);
+    emailService.sendEmail({
+      to: envs.DEFAULT_RECEIVER_EMAIL,
+      subject: 'System Logs',
+      htmlBody: `
+        <h3>NOC Report</h3>
+        <p>This is working, you are awesome!!!!
+      `
+    });
+  }
+
+  static sendLogFilesByEmail = (to: string) => {
+    const emailService = new EmailService(fileSystemLogRepository);
+    emailService.sendEmailWithSystemLogs(to);
   }
 }
