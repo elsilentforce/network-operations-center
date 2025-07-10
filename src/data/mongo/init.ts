7e8abec0520986e444da38f6e65c025200a88394
Patricio Tapia <native.devil@gmail.com>
2025-07-09 21:46:49 -0400

    REFACT: Mongo connection string

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
 interface ConnectionOptions {
-  mongoUrl: string;
+  mongoHost: string;
+  mongoUser: string;
+  mongoPassword: string;
   dbName: string;
 }
 
 export class MongoDatabase {
   static async connect(options: ConnectionOptions){
-    const { mongoUrl, dbName } = options;
+    const { mongoHost, dbName, mongoUser, mongoPassword } = options;
+
+    const conString =
+      `mongodb://${ mongoUser }:${ mongoPassword }@${ mongoHost }/${ dbName }?authSource=admin`;
 
     try{
-      await mongoose.connect( mongoUrl, {
-        dbName: dbName
-      });
+      await mongoose.connect(conString);
       console.log('Mongo connected');
     }catch(error){
       console.log('Mongo connection error');
