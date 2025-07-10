6463b1275ac5b86217be62c3c161c4549fc1bdfb
Patricio Tapia <native.devil@gmail.com>
2025-07-09 21:47:58 -0400

    ADD: MongoDB document insert

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/envs.plugins';
-import { MongoDatabase } from './data/mongo';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation/server';
 
 // Server execution start
@@ -10,9 +10,21 @@ import { Server } from './presentation/server';
 async function main(){
   // Initialize MongoDB connection
   await MongoDatabase.connect({
-    mongoUrl: envs.MONGO_URL,
+    mongoHost: envs.MONGO_HOST,
+    mongoUser: envs.MONGO_USER,
+    mongoPassword: envs.MONGO_PASSWORD,
     dbName: envs.MONGO_DB_NAME
   });
-
+  
   Server.start();
+  
+  const newLog = await LogModel.create({
+    message: 'Test message from Mongo',
+    origin: 'app.ts',
+    level: 'low'
+  });
+
+  await newLog.save();
+
+  console.log(newLog);
 }
