39e1e8cd2aefa73a644c40bfc4b17aeef5d1a6b1
Patricio Tapia <native.devil@gmail.com>
2025-07-11 22:00:57 -0400

    ADD: Tests on Mongo Model Schema

---
+++ src/data/mongo/models/log.model.test.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import { envs } from "../../../config/plugins/envs.plugins";
+import { MongoDatabase } from "../init";
+import { LogModel } from "./log.model";
+
+describe('LogModel', () => {
+  beforeAll( async() => {
+    await MongoDatabase.connect({
+      mongoHost: envs.MONGO_HOST,
+      mongoUser: envs.MONGO_USER,
+      mongoPassword: envs.MONGO_PASSWORD,
+      dbName: envs.MONGO_DB_NAME,
+    });
+  });
+
+  afterAll(()=>{
+    mongoose.connection.close();
+  });
+
+  it('returns LogModel', async() => {
+    const logData = {
+      origin: 'log.model.test.ts',
+      message: 'test-message',
+      level: 'low'
+    }
+
+    const log = await LogModel.create(logData);
+    expect(log).toEqual(expect.objectContaining({
+      ...logData,
+      createdAt: expect.any(Date),
+      id: expect.any(String)
+    }));
+
+    await LogModel.findByIdAndDelete( log.id );
+  });
+
+  it('returns valid schema object', () => {
+    const schema = LogModel.schema.obj;
+    expect(schema).toEqual(expect.objectContaining({
+      message: { type: expect.any(Function), required: true },
+      origin: { type: expect.any(Function) },
+      level: {
+        type: expect.any(Function),
+        enum: [ 'low', 'medium', 'high' ],
+        default: 'low'
+      },
+      createdAt: expect.any(Object)
+    }));
+  });
+});
