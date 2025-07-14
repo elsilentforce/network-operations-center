118feef0d29da10fc62e58452239a62ca895421d
Patricio Tapia <native.devil@gmail.com>
2025-07-14 13:04:31 -0400

    REFACT: Test hooks

--- src/infrastructure/datasources/mongo-log-datasource.test.ts
+++ src/infrastructure/datasources/mongo-log-datasource.test.ts
@@ -3,27 +3,26 @@ import { MongoLogDatasource } from "./mongo-log.datasource";
 import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 import { LogModel, MongoDatabase } from "../../data/mongo";
 import { envs } from "../../config/plugins/envs.plugins";
-import { LogDatasource } from "../../domain/datasources/log.datasource";
-
-afterEach(async()=>{
-  await LogModel.deleteMany();
-});
-
-afterAll(() => {
-  mongoose.connection.close();
-});
-
-const testLog = new LogEntity({
-        level: LogSeverityLevel.low,
-        message: 'test-message',
-        origin: 'mongo-log-datasourse.tests.ts'
-      });
-const mongoLogDatasource = new MongoLogDatasource();
 
 describe('MongoLogDataSource', () => {
   beforeAll(() => {
     MongoDatabase.connect(envs.MONGO_URL);
   });
+
+  afterEach(async()=>{
+    await LogModel.deleteMany();
+  });
+
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+
+  const testLog = new LogEntity({
+          level: LogSeverityLevel.low,
+          message: 'test-message',
+          origin: 'mongo-log-datasourse.tests.ts'
+        });
+  const mongoLogDatasource = new MongoLogDatasource();
 
   it('creates a Log', async() => {
     
